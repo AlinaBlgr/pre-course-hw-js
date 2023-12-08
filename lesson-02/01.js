8598e2132694448605d47ac6b7a839a4e996c7e3
Alina <linabtrf@gmail.com>
2023-12-09 00:47:13 +0200

    \\

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,8 +1,1 @@
-let number1 = Math.floor(Math.random() * 100);
-let result;
-if (number1 % 2 === 0) {
-    result = `${number1}: Это четное число`;
-} else {
-    result = `${number1}: Это нечетное число`;
-}
-console.log(result);
+// code
