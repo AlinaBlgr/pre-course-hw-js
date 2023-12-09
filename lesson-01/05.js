01b9ccfaeed223a623e8bb5ca048990016c13b2a
Alina <linabtrf@gmail.com>
2023-12-10 00:06:28 +0200

    lesson01. switch case (console.log once)

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,12 +1,14 @@
 let rule =
   "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
+  let answer;
 switch (true) {
   case rule.length < 25:
-    console.log("Все таки нет правил без исключения");
+    answer = "Все таки нет правил без исключения";
     break;
   case rule.length > 25:
-    console.log("У меня 100 пудов все получится");
+    answer = "У меня 100 пудов все получится";
     break;
   case (rule.length = 25):
-    console.log("50 на 50");
+    answer = "50 на 50";
 }
+console.log(answer);
