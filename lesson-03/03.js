74288a04ca2dba9bb6be107510e9960b89fe8335
Alina <linabtrf@gmail.com>
2024-01-07 00:29:03 +0200

    console

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,5 +1,3 @@
 const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
 
-const result = fruits.slice(1, -2);
-
-console.log(result);
+console.log(fruits.slice(1, -2));
